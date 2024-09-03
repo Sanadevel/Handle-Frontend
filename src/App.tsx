38258850ccd59958d3d59773139892aa60305b67
Sanadevel <a36133353@gmail.com>
2024-09-03 10:53:24 +0900

    🎉 : Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import WordleBlank from "./components/wordleBlank";
+import { wordleInputTypeArr } from "./types/wordleInput";
+import Keyboard from "./components/keyboard";
+import hangul from "hangul-js";
+import { exCorrect } from "./stores/constants/exampleData";
+import { checkWordle } from "./utils/checkWordle";
 
 function App() {
+  const [inputWordle, setInputWordle] = useState<wordleInputTypeArr>({
+    arr: [],
+    curIdx: 0,
+  });
+  const correctAwnser: wordleInputTypeArr = {
+    arr: hangul.d(exCorrect),
+    curIdx: 0,
+  };
+  /* const [awnserWordle, setAwnserWordle] = useState<awnserWordleType>({
+    arr: [],
+  }); */
+  const wordleLength = inputWordle.arr.length;
+
+  useEffect(() => {
+    setInputWordle({
+      arr: new Array(correctAwnser.arr.length).fill(""),
+      curIdx: 0,
+    });
+  }, [correctAwnser.arr.length]);
+
+  const changeWordle = (keyBoard: any) => {
+    const inputValue = keyBoard.target.value;
+    let newArr = inputWordle.arr;
+
+    if (inputValue === "입력" && inputWordle.curIdx === wordleLength) {
+      const _res = checkWordle({
+        awnser: inputWordle.arr,
+        correct: correctAwnser.arr,
+      });
+      console.log(_res);
+    } else if (inputValue === "지움" && inputWordle.curIdx > 0) {
+      newArr[inputWordle.curIdx - 1] = "";
+      setInputWordle({ arr: newArr, curIdx: inputWordle.curIdx - 1 });
+    } else if (
+      inputWordle.curIdx < wordleLength &&
+      !(inputValue === "지움") &&
+      !(inputValue === "입력")
+    ) {
+      newArr[inputWordle.curIdx] = inputValue;
+      setInputWordle({ arr: newArr, curIdx: inputWordle.curIdx + 1 });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WordleBlank inputWordle={correctAwnser} />
+      <WordleBlank inputWordle={inputWordle} />
+      <Keyboard changeWordle={changeWordle} />
+      <div>arr : {inputWordle?.arr.length}</div>
     </div>
   );
 }
---
+++ src/AppStyle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const app = styled.div`
+  text-align: center;
+  font-family: Pretendard-Regular;
+`;
