9db1818d068c1f52f7a4c776a62bc78fd40e96c4
Sanadevel <a36133353@gmail.com>
2024-09-03 11:11:50 +0900

    ✨ : Add Keyboard Components

---
+++ src/components/keyboard/index.tsx
@@ -0,0 +1,22 @@
+import { keyBoardWords } from "../../stores/constants/keyBoardConstant";
+import * as S from "./style";
+
+interface keyboardInterface {
+  changeWordle: (event: React.MouseEvent<HTMLInputElement>) => void;
+}
+
+const Keyboard = ({ changeWordle }: keyboardInterface) => {
+  return (
+    <S.KeyBoard>
+      {keyBoardWords.map((line: Array<string>) => (
+        <S.WordleLine>
+          {line.map((key: string) => (
+            <S.WordleBlank type="button" onClick={changeWordle} value={key} />
+          ))}
+        </S.WordleLine>
+      ))}
+    </S.KeyBoard>
+  );
+};
+
+export default Keyboard;
---
+++ src/components/keyboard/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const KeyBoard = styled.div`
+  width: 60em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const WordleLine = styled.div`
+  display: flex;
+`;
+
+export const WordleBlank = styled.input`
+  font-size: 1.5em;
+  width: 2em;
+  height: 2.5em;
+  border: 1px solid #000;
+  line-height: 2.5em;
+  font-family: Pretendard-Regular;
+  border-radius: 0.5em;
+  text-align: center;
+  margin: 0.3rem;
+  background: none;
+  cursor: pointer;
+`;
