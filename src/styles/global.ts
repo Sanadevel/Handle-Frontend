dd4779e33c753577a572ac594c57669801096917
Sanadevel <a36133353@gmail.com>
2024-09-10 19:51:24 +0900

    💄 : Add Color Palette

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,25 +1,30 @@
 import { createGlobalStyle } from "styled-components";
 
-const colors = {
-  green: "#6AAA64",
-  yellow: "#D1B036",
+export const Palette = {
+  gray_1: "#a9a9a9",
+  gray_2: "#949494",
+  gray_3: "#6a6a6a",
+  gray_4: "#3f3f3f",
+  black: "#000",
+  white: "#fff",
 };
 
 export const GlobalStyle = createGlobalStyle`
-@font-face {
+  @font-face {
+    font-family: "Pretendard-Regular";
+    src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  &:root{
+    background-color: #000;
+    color:#fff;
+  }
+
+  body{
   font-family: "Pretendard-Regular";
-  src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
-    format("woff");
-  font-weight: 400;
-  font-style: normal;
-}
-
-:root{
-  background-color: #000;
-  color:#fff;
-  --primary-green-color : #6AAA64
-  --primary-yellow-color : #D1B036
-}
-
-font-size: 16px;
+  font-size: 16px;
+  }
 `;
