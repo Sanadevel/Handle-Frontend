b4cb5cc4676ef110f9ffe77e2c7499d736c885a5
Sanadevel <a36133353@gmail.com>
2024-09-12 23:27:35 +0900

    ✨ : Update Input Component

--- src/components/mainpage/style.ts
+++ src/components/mainpage/style.ts
@@ -11,33 +11,3 @@ export const MainPage = styled.div`
 export const Title = styled.h1`
   font-size: 3rem;
 `;
-
-export const InputText = styled.input`
-  width: 50rem;
-  height: 2.5rem;
-  font-size: 1.25em;
-  font-family: "Pretendard-Regular";
-  text-align: center;
-  border: none;
-  border-bottom: 1px solid #fff;
-  background: none;
-  outline: none;
-  color: #fff;
-`;
-
-export const Button = styled.button`
-  width: 8rem;
-  height: 2.5rem;
-  font-size: 1.25em;
-  background-color: #000;
-  font-family: "Pretendard-Regular";
-  border: 1px solid #fff;
-  color: #fff;
-  margin-top: 1rem;
-  border-radius: 0.5em;
-  cursor: pointer;
-
-  &:hover {
-    background-color: var(--color-darkGray);
-  }
-`;
