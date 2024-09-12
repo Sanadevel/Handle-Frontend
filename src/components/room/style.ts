a7659d64fd0672ef6cbf7917415bf262f23ebf1b
Sanadevel <a36133353@gmail.com>
2024-09-12 23:28:07 +0900

    ✨ : Feat Room Page

--- src/components/room/style.ts
+++ src/components/room/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Palette } from "../../styles/global";
 
 export const Room = styled.div`
   width: 100vw;
@@ -6,4 +7,34 @@ export const Room = styled.div`
 
   display: flex;
   flex-direction: column;
+`;
+
+export const Header = styled.div`
+  height: 5vh;
+  border-bottom: 1px solid #fff;
+`;
+
+export const UsersTab = styled.div`
+  width: 100vw;
+  height: 65vh;
+  border-bottom: 1px solid #fff;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const User = styled.div`
+  width: 10em;
+  height: 30em;
+  border: 1px solid ${Palette.white};
+  text-align: center;
+  word-wrap: break-word;
+  line-height: 30em;
+`;
+
+export const Chat = styled.div`
+  height: 30vh;
+
+  display: flex;
+  flex-direction: column-reverse;
 `;
